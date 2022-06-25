bc0ccbf07274d65fabf262526f6d44a97bcc4bc2
Alan-aj <87690136+Alan-aj@users.noreply.github.com>
2022-06-25 15:09:58 +0530

    Update dbconnector.py

--- dbconnector.py
+++ dbconnector.py
@@ -3,32 +3,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# def iud(q,val):
-#      con=pymysql.connect(host='InventionandInventors.mysql.pythonanywhere-services.com',user='InventionandInve',passwd='Aswin123@#',db='InventionandInve$invention_and_inventors')
-#      cmd=con.cursor()
-#      cmd.execute(q,val)
-#      id=cmd.lastrowid
-#      con.commit()
-#      return id
-# def select(q):
-#     con=pymysql.connect(host='InventionandInventors.mysql.pythonanywhere-services.com',user='InventionandInve',passwd='Aswin123@#',db='InventionandInve$invention_and_inventors')
-#     cmd=con.cursor()
-#     cmd.execute(q)
-#     s=cmd.fetchall()
-#     return s
-# def selectall(q,val):
-#     con=pymysql.connect(host='InventionandInventors.mysql.pythonanywhere-services.com',user='InventionandInve',passwd='Aswin123@#',db='InventionandInve$invention_and_inventors')
-#     cmd=con.cursor()
-#     cmd.execute(q,val)
-#     s=cmd.fetchall()
-#     return s
-# def selectone(q,val):
-#     con=pymysql.connect(host='InventionandInventors.mysql.pythonanywhere-services.com',user='InventionandInve',passwd='Aswin123@#',db='InventionandInve$invention_and_inventors')
-#     cmd=con.cursor()
-#     cmd.execute(q,val)
-#     s=cmd.fetchone()
-#     return s
-
 
 def iud(q, val):
     con = pymysql.connect(host=os.getenv('HOST'), port=int(os.getenv('PORT')), user=os.getenv('USER'), passwd=os.getenv('PASSWD'), db=os.getenv('DB'))
